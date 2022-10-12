e3e22bf1eaf0b06c629e18381f202031df60e398
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2022-10-12 11:23:58 +0200

    BREAKING CHANGE: Add dependencies + web server operational

--- build.sbt
+++ build.sbt
@@ -1,8 +1,26 @@
-ThisBuild / version := "0.1.0-SNAPSHOT"
+ThisBuild / scalaVersion := "2.13.8"
 
-ThisBuild / scalaVersion := "3.2.0"
+val libVersion =
+  new {
+    val cassandra      = "4.0.6"
+    val javaSpark      = "2.9.4"
+    val kafka          = "3.3.1"
+    val scalatest      = "3.2.14"
+    val testcontainers = "1.17.5"
+  }
 
 lazy val root = (project in file("."))
   .settings(
     name := "scala_flux_rss"
   )
+
+libraryDependencies ++= Seq(
+  "org.scala-lang" % "scala-reflect" % scalaVersion.value,
+  "com.sparkjava"  % "spark-core" % libVersion.javaSpark,
+  "org.apache.kafka" % "kafka-clients" % libVersion.kafka,
+  "org.testcontainers" % "testcontainers" % libVersion.testcontainers,
+  "org.apache.cassandra" % "cassandra-all" % libVersion.cassandra,
+  "org.scalatest" %% "scalatest" % libVersion.scalatest % Test
+)
+
+Global / onChangedBuildSource := ReloadOnSourceChanges
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,55 @@
-@main
-def main(): Unit = {
-  println("Hello world!")
-  println("Hello test")
+import spark.Spark._
+import spark.{Request, Response}
+
+object main {
+    val serverPort = 8090
+
+    def main(args: Array[String]): Unit = {
+
+      port(serverPort)
+
+      get(
+        "/hello",
+        { (request: Request, response: Response) =>
+          response.`type`("application/json")
+
+          """{"message": "hello"}"""
+        }
+      )
+
+      get(
+        "/hello/:name",
+        { (request: Request, response: Response) =>
+          response.`type`("application/json")
+
+          val name = request.params("name")
+
+          s"""{"message": "hello $name"}"""
+        }
+      )
+
+      get(
+        "/message",
+        { (request: Request, response: Response) =>
+          response.`type`("application/json")
+
+          val content =
+            Option(
+              request.queryParams("content")
+            ).getOrElse("")
+
+          s"""{"message": "$content"}"""
+        }
+      )
+
+      get(
+        "/wrong",
+        { (request: Request, response: Response) =>
+          response.`type`("application/json")
+          response.status(404)
+
+          s"""{"message": "Not Found"}"""
+        }
+      )
+    }
 }
