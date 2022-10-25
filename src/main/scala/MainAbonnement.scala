fb32ad8a682113944a4921839b6be4a174dbd73f
Robin <robin.rieutord@gmail.com>
2022-10-25 21:47:21 +0200

    feat: add objet (MainAbonnement)

---
+++ src/main/scala/MainAbonnement.scala
@@ -0,0 +1,22 @@
+
+
+object MainAbonnement {
+  val serverPort = 8090
+  def main(args: Array[String]): Unit = {
+    val connection = Cassandra.connect()
+
+    connection.createKeyspace("my_keyspace")
+    connection.useKeyspace("my_keyspace")
+
+    Abonnement.createTableById(connection)
+    val abonnement = new Abonnement.Abonnement(0, "test.com")
+    abonnement.insert(connection)
+    val abonnement1 = new Abonnement.Abonnement(1, "toto.com")
+    abonnement.insert(connection)
+
+    val res = Abonnement.retrieveById(0)(connection)
+    val res1 = Abonnement.retrieveByFlux("toto.com")(connection)
+    println(res)
+    //println(res1)
+  }
+}
